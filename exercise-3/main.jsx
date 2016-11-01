23722ef8f86b3d4ccd5374146ec3ef5f0a341207
saniyamazman <mazmans@uw.edu>
2016-11-01 14:28:28 -0700

    participation

--- exercise-3/main.jsx
+++ exercise-3/main.jsx
@@ -9,7 +9,17 @@ var employeeData = [
 ];
 
 // Create an EmployeeRow element to render a row of information for an employee
+//<tr> makes a table for the employeeData table
+//<td> 
 var EmployeeRow = React.createClass({
+    render:function(){
+        return(
+            <tr> 
+                <td className={'name'}> {this.props.name} </td>
+                <td>{this.props.title}</td>
+                <td>{this.props.salary}</td>
+            </tr>)
+    }
     // Define rendering function
 });
 
@@ -21,16 +31,17 @@ var EmployeeTable = React.createClass({
                 <table>
                     <tbody>
                         <tr>
-                            <th>Name</th>
+                            <th>Name</th>   
                             <th>Title</th>
                             <th>Salary</th>
                         </tr>
-
-
-                        {// Enter employee rows here!!!!
+                        {
+                        // Enter employee rows here!!!!
+                        this.props.employeeData.map(function(data, index){
+                            return <EmployeeRow key={index} name={data.name} title={data.title}
+                            salary={data.salary}/> 
+                        })
                         }
-
-
                     </tbody>
                 </table>
             </div>
@@ -39,3 +50,7 @@ var EmployeeTable = React.createClass({
 });
 
 // Render your component in the `main` section
+
+ReactDOM.render(<EmployeeTable employeeData={employeeData}/>, //this line returns values from employeeData
+    document.querySelector('main')
+    );
